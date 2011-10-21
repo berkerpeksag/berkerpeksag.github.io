e93e4dbd4abc15edc20b0e147d26be082ae47cb6
Berker Peksag <berker.peksag@gmail.com>
2011-10-21 14:05:40 +0300

    Fixed slug regex.

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = patterns('blog.views',
     (r'^$', 'index'),
     (r'^archive/$', 'archive'),
-    (r'^(?P<slug>[a-z-]+)/$', 'detail'),
+    (r'^(?P<slug>[a-z0-9-]+)/$', 'detail'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
