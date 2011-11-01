792db3acdd5e17c84869350ebd4af92272769193
Berker Peksag <berker.peksag@gmail.com>
2011-11-01 02:36:42 +0200

    Added auto slug to post title.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,7 @@
 from blog.models import Post
 from django.contrib import admin
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Post, PostAdmin)
