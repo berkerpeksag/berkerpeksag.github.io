9eff53759b6ad14bbcc93933f3535399cfbbd619
Berker Peksag <berker.peksag@gmail.com>
2011-11-12 01:08:52 +0200

    Updated fabfile.

--- fabfile.py
+++ fabfile.py
@@ -2,30 +2,30 @@ from fabric.api import env, run, cd, sudo, put, require, settings, hide, puts
 from fabric.contrib import project, files
 
 env.project_name = 'berkerpeksag'
-env.root = '~/berkerpeksag'
+env.root = '~/'
 
 
 def deploy():
-    with cd(env.root):
-        sudo('git pull')
+    with cd('%(root)s%(project_name)s' % env):
+        run('git pull')
         put('conf/gunicorn.conf', '/etc/init/%(project_name)s.conf' %
                                   env, use_sudo=True)
-        sudo('initctl reload-configuration')
+        run('initctl reload-configuration')
 
 
 def start():
     """Start the Gunicorn process"""
-    sudo('start %(project_name)s' % env)
+    run('start %(project_name)s' % env)
 
 
 def stop():
     """Stop the Gunicorn process"""
-    sudo('stop %(project_name)s' % env)
+    run('stop %(project_name)s' % env)
 
 
 def reload():
     """Reload the Gunicorn configuration"""
-    sudo('reload %(project_name)s' % env)
+    run('reload %(project_name)s' % env)
 
 
 def restart():
@@ -36,18 +36,28 @@ def restart():
 
 def status():
     """Status of Gunicorn"""
-    sudo('status %(project_name)s' % env)
+    run('status %(project_name)s' % env)
 
 
 def update_dependencies():
     """Update requirements remotely."""
     put('requirements.txt', '%(root)s/requirements.txt' % env, use_sudo=True)
-    sudo('%(root)s/bin/pip install -r %(root)s/requirements.txt' % env)
+    run('%(root)s/bin/pip install -r %(root)s/requirements.txt' % env)
+
+
+def configure():
+    with cd(env.root):
+        run('git clone git://github.com/berkerpeksag/berkerpeksag.git')
+
+    with cd('%(root)s%(project_name)s' % env):
+        run('virtualenv --no-site-packages .')
+        run('source bin/activate')
+        put('conf/gunicorn.conf', '/etc/init/%(project_name)s.conf' % env)
 
 
 def setup():
-    sudo('virtualenv --no-site-packages %(project_name)s' % env)
+    pass
 
-    with cd(env.root):
-        sudo('source bin/activate')
-        sudo('git clone git://github.com/berkerpeksag/berkerpeksag.git .')
+
+def hello():
+    print 'World'
