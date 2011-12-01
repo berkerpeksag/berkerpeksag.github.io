cd78c951f7bdd02c4f8c3502c6629da08c994559
Berker Peksag <berker.peksag@gmail.com>
2011-12-02 00:31:27 +0200

    RTFM: isfile -> exists

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os, sys
 if __name__ == "__main__":
     settings_local_path = 'berkerpeksag/settings_local.py'
     settings_name = 'berkerpeksag.settings'
-    settings = settings_name + '_local' if os.path.isfile(settings_local_path) else settings_name
+    settings = settings_name + '_local' if os.path.exists(settings_local_path) else settings_name
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
     from django.core.management import execute_from_command_line
