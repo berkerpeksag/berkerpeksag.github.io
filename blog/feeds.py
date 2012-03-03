d4b0b25338929d298ffca1fc02402c9f74a0c474
Berker Peksag <berker.peksag@gmail.com>
2012-03-03 23:29:23 +0200

    Added missing feeds.py

---
+++ blog/feeds.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from django.contrib.syndication.views import Feed
+from blog.models import Post
+
+
+class LatestEntriesFeed(Feed):
+
+    def title(self):
+        return 'Berker Peksag'
+
+    def description(self):
+        return 'Latest posts from berkerpeksag.com'
+
+    def link(self):
+        return '/'
+
+    def feed_url(self):
+        return '/feed/'
+
+    def items(self):
+        return Post.objects.order_by('-pub_date')[:5]
+
+    def item_author_name(self):
+        return 'Berker Peksag'
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.body
