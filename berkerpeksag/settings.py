f8e100afd2dcbde7dc98f5a4b3a35f24ee325a15
Berker Peksag <berker.peksag@gmail.com>
2012-03-05 15:35:17 +0200

    Removed unused apps.

--- berkerpeksag/settings.py
+++ berkerpeksag/settings.py
@@ -115,14 +115,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
     'django.contrib.markup',
     'gunicorn',
     'south',
