c26828e0c034421423068d41a9488c9a0bfd485f
Berker Peksag <berker.peksag@gmail.com>
2012-03-05 15:36:28 +0200

    Removed unused context processors.

--- berkerpeksag/settings.py
+++ berkerpeksag/settings.py
@@ -103,11 +103,9 @@ TEMPLATE_DIRS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.contrib.auth.context_processors.auth',
-    'django.contrib.messages.context_processors.messages',
     'django.core.context_processors.request',
     'blog.context_processors.debug_mode',
 )
