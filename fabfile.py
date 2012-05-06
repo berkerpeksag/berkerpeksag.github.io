3cfa05bcf94c1c110d6436be496c1e62315a44b7
Berker Peksag <berker.peksag@gmail.com>
2012-05-06 14:36:52 +0300

    Use --ignore-installed option.

--- fabfile.py
+++ fabfile.py
@@ -50,7 +50,7 @@ def static():
 
 def update_dependencies():
     """Update requirements remotely."""
-    run('%(root)s%(project_name)s/bin/pip install -I -r %(root)s%(project_name)s/requirements.txt' % env)
+    run('%(root)s%(project_name)s/bin/pip install -r %(root)s%(project_name)s/requirements.txt --ignore-installed' % env)
 
 
 def configure():
