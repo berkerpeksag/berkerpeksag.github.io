b40e6fbc164e7af423d9f60066f6a8cc7f12fc83
Berker Peksag <berker.peksag@gmail.com>
2012-05-06 14:40:01 +0300

    Minor update.

--- fabfile.py
+++ fabfile.py
@@ -50,7 +50,7 @@ def static():
 
 def update_dependencies():
     """Update requirements remotely."""
-    run('%(root)s%(project_name)s/bin/pip install -r %(root)s%(project_name)s/requirements.txt --ignore-installed' % env)
+    run('%(root)s%(project_name)s/bin/pip install -ir %(root)s%(project_name)s/requirements.txt' % env)
 
 
 def configure():
