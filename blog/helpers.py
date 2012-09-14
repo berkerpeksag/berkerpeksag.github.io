67a9b7e8ea54a0c67736e60c32bf83be04810609
Berker Peksag <berker.peksag@gmail.com>
2012-09-14 12:37:43 +0300

    Use the render decorator instead of the render helper.

--- blog/helpers.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-
-def render(template, params, request):
-    return render_to_response('{0:s}.html'.format(template),
-                              params,
-                              context_instance=RequestContext(request))
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import get_object_or_404
 
-from blog.helpers import render
+from blog.decorators import render
 from blog.models import Post
 
 
+@render('blog/index')
 def index(request):
     blogs = Post.objects.filter(status=True, archive=False)[:5]
-    return render('blog/index', {'blogs': blogs}, request)
+    return {'blogs': blogs}
 
 
+@render('blog/detail')
 def detail(request, slug):
     blog = get_object_or_404(Post, slug=slug)
-    return render('blog/detail', {'blog': blog}, request)
+    return {'blog': blog}
 
 
+@render('blog/archive')
 def archive(request):
     archive = Post.objects.filter(status=True,
         archive=False).values('slug', 'title', 'pub_date')
-    return render('blog/archive', {'archive': archive}, request)
+    return {'archive': archive}
