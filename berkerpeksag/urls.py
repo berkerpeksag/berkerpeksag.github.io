2a1ca6ae3f3ffa9a976e0099dd9a6461378c4057
Berker Peksag <berker.peksag@gmail.com>
2013-01-27 23:52:03 +0200

    django.conf.urls.defaults is now deprecated.
    Use django.conf.urls instead. The current usage will be removed
    in Django 1.6.
    
    Closes #8.

--- berkerpeksag/urls.py
+++ berkerpeksag/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, patterns
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
