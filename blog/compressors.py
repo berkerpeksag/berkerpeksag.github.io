6b8ec2f6391e0d6162a72a872b1a6c6c9cb91f69
Berker Peksag <berker.peksag@gmail.com>
2019-04-20 23:46:14 +0300

    Delete Django application

--- blog/compressors.py
+++
@@ -1,8 +0,0 @@
-from cssmin import cssmin
-from pipeline.compressors import CompressorBase
-
-
-class CssminCompressor(CompressorBase):
-
-    def compress_css(self, css):
-        return cssmin(css)
--- conf/gunicorn.py
+++
@@ -1,6 +0,0 @@
-import multiprocessing
-
-bind = '127.0.0.1:8888'
-logfile = "/tmp/blog.log"
-workers = multiprocessing.cpu_count() * 2 + 1
-debug = False
