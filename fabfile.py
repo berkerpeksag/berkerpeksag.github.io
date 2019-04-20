d7b0f13ae297847d5f7eebf3b596ecb0907c0e4d
Berker Peksag <berker.peksag@gmail.com>
2019-04-20 23:22:41 +0300

    Cleanup requirements files and fabfile

--- fabfile.py
+++ fabfile.py
@@ -25,6 +25,7 @@ def deploy():
     with venv():
         run('git pull')
 
+    static()
     restart_nginx()
 
 
@@ -36,67 +37,4 @@ def restart_nginx():
 def static():
     """Update static files."""
     with venv():
-        sudo('rm -r static/')
-        run('source bin/activate')
-        sudo('python manage.py collectstatic --noinput')
-
-
-
-def configure():
-    """Configure basic tools."""
-    with cd(env.root):
-        run('git clone git://github.com/berkerpeksag/berkerpeksag.git')
-        run('virtualenv venv')
-        run('source venv/bin/activate')
-
-    with venv():
-        run('pip install -r requirements.txt')
-        static()
-        sudo('ln -s /home/wakefield/berkerpeksag/conf/nginx.conf /etc/nginx/sites-enabled/berkerpeksag.com')
-        run('echo_supervisord_conf > supervisord.conf')
-        sudo('mv supervisord.conf /etc/supervisord.conf')
-        sudo('cat conf/supervisor.conf >> /etc/supervisord.conf')
-        run('supervisord')
-
-    restart_nginx()
-
-
-def setup():
-    """Setup the VM."""
-    sudo('apt-get update && apt-get upgrade && apt-get install git-core sqlite3 '
-         'python-sqlite python-setuptools python-pip python-dev build-essential '
-         'nginx curl libcurl3')
-    sudo('pip install virtualenv fabric')
-
-
-def clean():
-    """Clean the current setup."""
-    stop()
-    sudo('unlink /tmp/supervisor.sock')
-
-    with cd(env.root):
-        sudo('rm -r berkerpeksag/')
-        sudo('rm /etc/nginx/sites-enabled/berkerpeksag.com')
-        sudo('rm /etc/supervisord.conf')
-
-
-def install():
-    """Configures the development environment."""
-    local('virtualenv venv')
-    with venv():
-        local('pip install -r requirements-dev.txt')
-        local('cp berkerpeksag/settings_local.py.dist berkerpeksag/settings_local.py')
-        local('python manage.py syncdb')
-
-
-# Development
-
-
-def server(port='8000'):
-    """Starts development server."""
-    local('python manage.py runserver %s' % port)
-
-
-def clean_pyc():
-    """Remove all .pyc files."""
-    local('find . -name "*.pyc" -exec rm {} \;')
+        put('build', 'build')
